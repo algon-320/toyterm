7d8c49691c7382dfc56c4ae6494abf9fa2aa84f3
algon-320 <algon.0320@gmail.com>
2022-07-03 04:41:48 +0900

    Add utilities

--- src/utils.rs
+++ src/utils.rs
@@ -171,3 +171,135 @@ pub mod utf8 {
         }
     }
 }
+
+pub mod extension {
+    pub trait GetMutPair<T> {
+        fn get_mut_pair(&mut self, a: usize, b: usize) -> (&mut T, &mut T);
+    }
+
+    impl<T> GetMutPair<T> for [T] {
+        fn get_mut_pair(&mut self, a: usize, b: usize) -> (&mut T, &mut T) {
+            assert!(a != b && a < self.len() && b < self.len());
+
+            use std::cmp::{max, min};
+            let (a, b, swapped) = (min(a, b), max(a, b), a > b);
+
+            // <--------xyz-------->
+            // <--x--><-----yz----->
+            //        <--y--><--z-->
+            // .......a......b......
+            let xyz = self;
+            let (_x, yz) = xyz.split_at_mut(a);
+            let (y, z) = yz.split_at_mut(b - a);
+            let mut1 = &mut y[0];
+            let mut2 = &mut z[0];
+
+            if swapped {
+                (mut2, mut1)
+            } else {
+                (mut1, mut2)
+            }
+        }
+    }
+
+    impl<T> GetMutPair<T> for std::collections::VecDeque<T> {
+        fn get_mut_pair(&mut self, a: usize, b: usize) -> (&mut T, &mut T) {
+            assert!(a != b && a < self.len() && b < self.len());
+
+            let (fst, snd) = self.as_mut_slices();
+
+            use std::cmp::{max, min};
+            let (a, b, swapped) = (min(a, b), max(a, b), a > b);
+            debug_assert!(a < b);
+
+            let (mut1, mut2) = if b < fst.len() {
+                debug_assert!(a < fst.len() && b < fst.len());
+                let i = a;
+                let j = b;
+                fst.get_mut_pair(i, j)
+            } else if fst.len() <= a {
+                debug_assert!(fst.len() <= a && fst.len() <= b);
+                let i = a - fst.len();
+                let j = b - fst.len();
+                snd.get_mut_pair(i, j)
+            } else {
+                debug_assert!(a < fst.len() && (b - fst.len()) < snd.len());
+                let i = a;
+                let j = b - fst.len();
+                (&mut fst[i], &mut snd[j])
+            };
+
+            if swapped {
+                (mut2, mut1)
+            } else {
+                (mut1, mut2)
+            }
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_slice() {
+            let mut a = [0, 1, 2, 3, 4];
+            let slice = a.as_mut_slice();
+
+            let (x, y) = slice.get_mut_pair(0, 1);
+            assert_eq!(*x, 0);
+            assert_eq!(*y, 1);
+
+            let (x, y) = slice.get_mut_pair(1, 0);
+            assert_eq!(*x, 1);
+            assert_eq!(*y, 0);
+
+            let (x, y) = slice.get_mut_pair(0, 4);
+            assert_eq!(*x, 0);
+            assert_eq!(*y, 4);
+
+            let (x, y) = slice.get_mut_pair(4, 0);
+            assert_eq!(*x, 4);
+            assert_eq!(*y, 0);
+        }
+
+        #[test]
+        fn test_deque() {
+            use std::collections::VecDeque;
+
+            let mut deq = VecDeque::with_capacity(5);
+            deq.push_back(2);
+            deq.push_back(3);
+            deq.push_back(4);
+            deq.push_front(1);
+            deq.push_front(0);
+
+            assert_eq!(deq[0], 0);
+            assert_eq!(deq[1], 1);
+            assert_eq!(deq[2], 2);
+            assert_eq!(deq[3], 3);
+            assert_eq!(deq[4], 4);
+
+            let (x, y) = deq.get_mut_pair(0, 1);
+            assert_eq!(*x, 0);
+            assert_eq!(*y, 1);
+            let (x, y) = deq.get_mut_pair(1, 0);
+            assert_eq!(*x, 1);
+            assert_eq!(*y, 0);
+
+            let (x, y) = deq.get_mut_pair(1, 2);
+            assert_eq!(*x, 1);
+            assert_eq!(*y, 2);
+            let (x, y) = deq.get_mut_pair(2, 1);
+            assert_eq!(*x, 2);
+            assert_eq!(*y, 1);
+
+            let (x, y) = deq.get_mut_pair(2, 3);
+            assert_eq!(*x, 2);
+            assert_eq!(*y, 3);
+            let (x, y) = deq.get_mut_pair(3, 2);
+            assert_eq!(*x, 3);
+            assert_eq!(*y, 2);
+        }
+    }
+}
